9a00822a68571d0bd7924707e6c3094e342502bd
Hogyun Kim <ggyuniH0@gmail.com>
2022-06-15 09:54:05 +0900

    #11 JasyptTest Encrypt & Decrypt 테스트

---
+++ src/test/java/com/sparta/week02homework/JasyptTest.java
@@ -0,0 +1,37 @@
+package com.sparta.week02homework;
+
+
+import org.jasypt.encryption.pbe.PooledPBEBigDecimalEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class JasyptTest {
+    @Test
+    public void jasypt_test() {
+        String plainText = "H2jAB61fN0aTDZE+/vAP55heJbsxw2I/ZiOgZIZi";
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("password");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        String encryptText = encryptor.encrypt(plainText);
+        System.out.println(encryptText);
+        String decryptText = encryptor.decrypt(encryptText);
+        System.out.println(decryptText);
+
+        assertThat(plainText).isEqualTo(decryptText);
+
+    }
+
+}
