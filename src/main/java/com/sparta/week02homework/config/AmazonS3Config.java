d1e1a69a2f05d6c06a0b53dd019214e42b7b010d
Hogyun Kim <ggyuniH0@gmail.com>
2022-06-16 16:35:02 +0900

    #9 AmazonS3Config.java 오타수정

--- src/main/java/com/sparta/week02homework/config/AmazonS3Config.java
+++ src/main/java/com/sparta/week02homework/config/AmazonS3Config.java
@@ -14,7 +14,7 @@ public class AmazonS3Config {
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
