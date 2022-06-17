aa2de8ffeb7d6686ca93ebb82d9731e16cfeeea6
Hogyun Kim <ggyuniH0@gmail.com>
2022-06-17 19:43:10 +0900

    #10 ErrorResult.java

---
+++ src/main/java/com/sparta/week02homework/exception/ErrorResult.java
@@ -0,0 +1,12 @@
+package com.sparta.week02homework.exception;
+
+
+import lombok.AllArgsConstructor;
+
+
+
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
